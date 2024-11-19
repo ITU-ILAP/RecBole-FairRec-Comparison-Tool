0c6555568271fc78df7ab90f4b27434e4716ba80
Umutcan Karakas <umutcankarakas@gmail.com>
2024-11-19 18:46:08 +0300

    Subsets filtered and added

--- recbole/utils/dataset_utils.py
+++ recbole/utils/dataset_utils.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
 
-def split_and_save_dataset(input_file_path, output_folder_path):
+def split_save_dataset(input_file_path, output_folder_path):
     # Read the dataset
-    df = pd.read_csv(input_file_path, sep='\t', header=0, names=['user_id', 'item_id', 'rating', 'timestamp'])
+    df = pd.read_csv(input_file_path, sep='\t', header=0, names=['user_id:token', 'item_id:token', 'rating:float', 'timestamp:float'])
     subset_size = len(df) // 10
 
     # Create subsets
@@ -20,24 +20,40 @@ def split_and_save_dataset(input_file_path, output_folder_path):
 
     # Save each subset
     for i, subset in enumerate(subsets):
-        subset_filename = f'{os.path.splitext(os.path.basename(input_file_path))[0]}_subset_{i+1}' #.csv 
+        subset_filename = f'subset_{i+1}.inter'
         output_file_path = os.path.join(output_folder_path, subset_filename)
         subset.to_csv(output_file_path, index=False, sep='\t')
 
     print(f'Subsets saved in {output_folder_path}')
 
-def process_datasets(main_folder):
-    # Walk through each folder in the main directory
-    for root, dirs, files in os.walk(main_folder):
-        for file in files:
-            if 'inter' in file:
-                input_file_path = os.path.join(root, file)
-                output_folder_path = os.path.join(root, 'inter_subsets')
-                try:
-                    split_and_save_dataset(input_file_path, output_folder_path)
-                except:
-                    continue
 
+def split_filter_save_dataset(input_file_path, output_folder_path):
+    # Read the dataset
+    df = pd.read_csv(input_file_path, sep='\t', header=0, names=['user_id:token', 'item_id:token', 'rating:float', 'timestamp:float'])
+    subset_size = len(df) // 10
+
+    # Create subsets and apply filter
+    for i in range(10):
+        start_index = i * subset_size
+        if i == 9:
+            subset = df.iloc[start_index:]
+        else:
+            subset = df.iloc[start_index:start_index + subset_size]
+        
+        # Filter items occurring less than 10 times
+        item_counts = subset['item_id:token'].value_counts()
+        items_to_keep = item_counts[item_counts >= 10].index
+        filtered_subset = subset[subset['item_id:token'].isin(items_to_keep)]
+        
+        # Ensure the output folder exists
+        os.makedirs(output_folder_path, exist_ok=True)
+
+        # Save the filtered subset
+        subset_filename = f'subset_{i+1}.inter'
+        output_file_path = os.path.join(output_folder_path, subset_filename)
+        filtered_subset.to_csv(output_file_path, index=False, sep='\t')
+
+    print(f'Filtered subsets saved in {output_folder_path}')
                 
                 
 
--- run_dataset_split.py
+++ run_dataset_split.py
@@ -1,4 +1,8 @@
-from recbole.utils.dataset_utils import process_datasets
+from recbole.utils.dataset_utils import split_save_dataset, split_filter_save_dataset
 
-main_folder_path = 'dataset_v2'
-process_datasets(main_folder_path)
+input_file_path = 'dataset_v2\ml-1M\ml-1M.inter'
+output_folder_path = 'dataset_v2\ml-1M\inter_subsets'
+split_save_dataset(input_file_path, output_folder_path)
+
+filtered_output_folder_path = 'dataset_v2\ml-1M\inter_subsets_filtered'
+split_filter_save_dataset(input_file_path, filtered_output_folder_path)
